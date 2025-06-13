f23e9d2ca39ec739e263670d65bb4496ae7f53b3
daveee00 <davide.rebizzi1408@gmail.com>
2025-06-13 13:36:22 +0200

    Update previous.js

--- ftc/scripts/previous.js
+++ ftc/scripts/previous.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add click event listener to navigate
   container.addEventListener("click", function() {
     window.location.href =
-      "https://wddc-slipknot.webflow.io/performances/radio-pirate";
+      "https://wddc-slipknot.webflow.io/performances/endovena-festival";
   });
 
   let clock = new THREE.Clock();
