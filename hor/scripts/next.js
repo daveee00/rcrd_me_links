434f569fe8135e996dafe86ca12dedeff7146c91
daveee00 <davide.rebizzi1408@gmail.com>
2025-06-29 13:09:30 +0200

    Update next.js

--- hor/scripts/next.js
+++ hor/scripts/next.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add click event listener to navigate
   container.addEventListener("click", function () {
     window.location.href =
-      "https://wddc-slipknot.webflow.io/performances/radio-pirate";
+      "https://wddc-slipknot.webflow.io/performances/the-lot-radio";
   });
 
   let clock = new THREE.Clock();
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Model loader
   const loader = new THREE.GLTFLoader();
   loader.load(
-    "https://rcrdme-gnmr.netlify.app/blender-exp/radiopirate.glb",
+    "https://rcrdme-gnmr.netlify.app/blender-exp/thelotradio.glb",
     function (gltf) {
       model = gltf.scene;
       model.scale.set(10, 10, 10);
