321db2d668ac37bb1a1d8d3da0ba7d75f59f075d
daveee00 <davide.rebizzi1408@gmail.com>
2025-06-30 13:06:42 +0200

    Update disco.js

--- lot-radio/scripts/disco.js
+++ lot-radio/scripts/disco.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Model loader
   const loader = new THREE.GLTFLoader();
   loader.load(
-    "https://rcrdme-gnmr.netlify.app/blender-exp/hor-disco3.glb", // <-- update this path if needed
+    "https://rcrdme-gnmr.netlify.app/blender-exp/lotradio-disco.glb", // <-- update this path if needed
     function (gltf) {
       model = gltf.scene;
       model.scale.set(11, 11, 11);
